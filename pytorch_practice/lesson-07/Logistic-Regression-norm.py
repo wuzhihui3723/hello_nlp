ab0ee908031f65a66a39a6b83f5d78f074b2783c
wuzhihui <wuzhihui1018@163.com>
2022-05-11 10:59:07 +0800

    历史项目整理3

--- pytorch_practice/lesson-07/Logistic-Regression-norm.py
+++ pytorch_practice/lesson-07/Logistic-Regression-norm.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 """
-# @file name  : lesson-05-Logsitic-Regression.py
+# @file name  : Logsitic-Regression-norm.py
 # @author     : wuzhihui
-# @date       : 2019-09-03 10:08:00
-# @brief      : 逻辑回归模型训练
-机器学习模型训练步骤：数据、模型、损失函数、优化器、迭代训练
+# @date       : 2019-09-08 10:08:00
+# @brief      :
 """
 import torch
 import torch.nn as nn
@@ -12,11 +11,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 torch.manual_seed(10)
 
+lr = 0.01  # 学习率
 
-# ============================ step 1/5 生成数据 ============================
+# 生成虚拟数据
 sample_nums = 100
 mean_value = 1.7
-bias = 1
+bias = 5         # 5
 n_data = torch.ones(sample_nums, 2)
 x0 = torch.normal(mean_value * n_data, 1) + bias      # 类别0 数据 shape=(100, 2)
 y0 = torch.zeros(sample_nums)                         # 类别0 标签 shape=(100, 1)
@@ -26,7 +26,7 @@ train_x = torch.cat((x0, x1), 0)
 train_y = torch.cat((y0, y1), 0)
 
 
-# ============================ step 2/5 选择模型 ============================
+# 定义模型
 class LR(nn.Module):
     def __init__(self):
         super(LR, self).__init__()
@@ -39,24 +39,19 @@ class LR(nn.Module):
         return x
 
 
-lr_net = LR()   # 实例化逻辑回归模型
+lr_net = LR()
 
+# 定义损失函数与优化器
+loss_fn = nn.BCELoss()
+optimizer = torch.optim.SGD(lr_net.parameters(), lr=0.01, momentum=0.9)
 
-# ============================ step 3/5 选择损失函数 ============================
-loss_fn = nn.BCELoss() #二分类的交叉熵
-
-# ============================ step 4/5 选择优化器   ============================
-lr = 0.01  # 学习率
-optimizer = torch.optim.SGD(lr_net.parameters(), lr=lr, momentum=0.9)
-
-# ============================ step 5/5 模型训练 ============================
 for iteration in range(1000):
 
     # 前向传播
     y_pred = lr_net(train_x)
 
-    # 计算 loss
-    loss = loss_fn(y_pred.squeeze(), train_y)
+    # 计算 MSE loss
+    loss = loss_fn(y_pred, train_y)
 
     # 反向传播
     loss.backward()
@@ -68,11 +63,11 @@ for iteration in range(1000):
     optimizer.zero_grad()
 
     # 绘图
-    if iteration % 20 == 0:
+    if iteration % 40 == 0:
 
         mask = y_pred.ge(0.5).float().squeeze()  # 以0.5为阈值进行分类
         correct = (mask == train_y).sum()  # 计算正确预测的样本个数
-        acc = correct.item() / train_y.size(0)  # 计算分类准确率
+        acc = correct.item() / train_y.size(0)  # 计算精度
 
         plt.scatter(x0.data.numpy()[:, 0], x0.data.numpy()[:, 1], c='r', label='class 0')
         plt.scatter(x1.data.numpy()[:, 0], x1.data.numpy()[:, 1], c='b', label='class 1')
@@ -83,8 +78,8 @@ for iteration in range(1000):
         plot_x = np.arange(-6, 6, 0.1)
         plot_y = (-w0 * plot_x - plot_b) / w1
 
-        plt.xlim(-5, 7)
-        plt.ylim(-7, 7)
+        plt.xlim(-5, 10)
+        plt.ylim(-7, 10)
         plt.plot(plot_x, plot_y)
 
         plt.text(-5, 5, 'Loss=%.4f' % loss.data.numpy(), fontdict={'size': 20, 'color': 'red'})
